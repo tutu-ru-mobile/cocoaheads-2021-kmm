1c3459446545badc0f91c855d2da5678fcd04d97
avdim <avdim@mail.ru>
2021-10-24 18:08:01 +0300

    RenderNode

--- iosApp/src/iosAppMain/apple/ContentView.swift
+++ iosApp/src/iosAppMain/apple/ContentView.swift
@@ -29,14 +29,67 @@ struct RenderNode: View {
     var body: some View {
         if (node is Node.Container.ContainerV) {
             let v = node as! Node.Container.ContainerV
-            List(v.children, id: \.key ,rowContent: {data in RenderNode(node: data)})
-//                List(["1","2","3"], id: \.base) { toggle in
-//                    Text("element \(data)")
-//                }
-//                Text("\(v.children.count)")
+            List(v.children, id: \.key, rowContent: { data in RenderNode(node: data) })
+        } else if (node is Node.Container.ContainerH) {
+            let v = node as! Node.Container.ContainerH
+            List(v.children, id: \.key, rowContent: { data in RenderNode(node: data) })
         } else if (node is Node.Leaf.LeafLabel) {
             let label = node as! Node.Leaf.LeafLabel
             Text(label.text)
+        } else if (node is Node.Leaf.LeafRectangle) {
+            let rect = node as! Node.Leaf.LeafRectangle
+            Rectangle()
+                    .fill(Color.red)
+                    .frame(width: CGFloat(rect.width), height: CGFloat(rect.height))
+        } else if (node is Node.Leaf.LeafButton) {
+            let button = node as! Node.Leaf.LeafButton
+            Button(action: {
+                print("Button pressed")
+                //todo send intent
+            }) {
+                Text(button.text)
+            }
+        } else if (node is Node.Leaf.LeafImage) {
+            let img = node as! Node.Leaf.LeafImage
+            Rectangle()
+                    .fill(Color.green)
+                    .frame(width: CGFloat(img.width), height: CGFloat(img.height))
+        } else if (node is Node.Leaf.LeafInput) {
+            let input = node as! Node.Leaf.LeafInput
+            InputTextView(label: input.hint, value: "TODO") { inputValueStr in
+                print(inputValueStr)
+                //todo save storage by key
+            }
         }
+    }
+}
+
+struct InputTextView: View {
+    var label: String
+    var onEdit: (String) -> Void
+    var value: String
+
+    public init(label: String, value: String, onEdit: @escaping (String) -> ()) {
+        self.label = label
+        self.onEdit = onEdit
+        self.value = value
+    }
+
+    func getBoundValue() -> Binding<String> {
+        Binding<String>(get: { () -> String in
+            self.value
+        }, set: { s in
+            self.onEdit(s)
+        })
+    }
+
+    public var body: some View {
+        HStack {
+            Text(label)
+            TextField("почта", text: getBoundValue())
+                    .font(Font.system(size: 15, weight: .medium, design: .serif))
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+        }
+                .padding()
     }
 }
