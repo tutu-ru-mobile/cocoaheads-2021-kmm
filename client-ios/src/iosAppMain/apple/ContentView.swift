1d4b268befdb922c677a4b8168521d54177dbcc7
avdeev <avdeev@tutu.tech>
2021-10-26 09:47:17 +0300

    TabView

--- client-ios/src/iosAppMain/apple/ContentView.swift
+++ client-ios/src/iosAppMain/apple/ContentView.swift
@@ -2,7 +2,21 @@ import SwiftUI
 import Shared
 
 struct ContentView: View {
+
+    @State private var selectedTab = 0
+
     var body: some View {
-        RenderRefreshView()
+        TabView(selection: $selectedTab) {
+            RenderRefreshView().tabItem {
+                Label("Главная", systemImage: "house.fill")
+            }
+            Text("Todo2").tabItem {
+                Label("Билеты", systemImage: "list.dash")
+            }
+            Text("Todo").tabItem {
+                Label("Помощь", systemImage: "phone.fill")
+            }
+        }
+
     }
 }
