0c0f45f74fd81bb990eac2c9b82871acc7d2a2d6
avdim <avdim@mail.ru>
2021-10-26 19:51:22 +0300

    client-android compose

--- client-android/build.gradle.kts
+++ client-android/build.gradle.kts
@@ -1,40 +1,16 @@
 plugins {
-    kotlin("multiplatform")
     id("com.android.application")
-    id("kotlin-android-extensions")
-}
-
-repositories {
-    google()
-    jcenter()
-}
-
-kotlin {
-    android()
-    sourceSets {
-        val androidMain by getting {
-            dependencies {
-                implementation(project(":shared"))
-                implementation("com.google.android.material:material:1.2.1")
-                implementation("androidx.appcompat:appcompat:1.2.0")
-                implementation("androidx.constraintlayout:constraintlayout:2.0.2")
-            }
-        }
-    }
+    kotlin("android")
 }
 
 android {
-    compileSdkVersion(ANDROID_COMPILE_SDK)
+    compileSdk = ANDROID_COMPILE_SDK
     defaultConfig {
         applicationId = "ru.tutu.androidApp"
-        minSdkVersion(ANDROID_MIN_SDK)
-        targetSdkVersion(ANDROID_TARGET_SDK)
+        minSdk = ANDROID_MIN_SDK
+        targetSdk = ANDROID_TARGET_SDK
         versionCode = 1
         versionName = "1.0"
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
     }
     buildTypes {
         getByName("release") {
@@ -44,4 +20,42 @@ android {
             isMinifyEnabled = false
         }
     }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+        freeCompilerArgs = freeCompilerArgs + "-Xopt-in=kotlin.RequiresOptIn"
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = COMPOSE_ANDROID
+    }
+}
+
+dependencies {
+    implementation(project(":shared"))
+    implementation("com.google.android.material:material:1.2.1")
+    implementation("androidx.appcompat:appcompat:1.2.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.0.2")
+
+    implementation("androidx.compose.ui:ui:$COMPOSE_ANDROID")
+    implementation("androidx.compose.ui:ui-tooling:$COMPOSE_ANDROID")
+    implementation("androidx.compose.foundation:foundation:$COMPOSE_ANDROID")
+    implementation("androidx.compose.material:material:$COMPOSE_ANDROID")
+    implementation("androidx.activity:activity-compose:$COMPOSE_ANDROID")
+    //Compose Utils
+    implementation("io.coil-kt:coil-compose:1.4.0")
+    implementation("com.google.accompanist:accompanist-insets:0.20.0")
+    implementation("com.google.accompanist:accompanist-swiperefresh:0.20.0")
+    //Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$COROUTINE_VERSION")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$COROUTINE_VERSION")
+    //DI
+    implementation("io.insert-koin:koin-core:3.1.2")
+    implementation("io.insert-koin:koin-android:3.1.2")
+    //Navigation
+    implementation("cafe.adriel.voyager:voyager-navigator:1.0.0-beta13")
+    //WorkManager
+    implementation("androidx.work:work-runtime-ktx:2.7.0")
 }
